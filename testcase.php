f3ab5703ec01b526b9a0f12c2d763556bf12964b
AnrDaemon <anrdaemon@yandex.ru>
2019-07-19 00:06:07 +0300

    Testcase for https://www.smarty.net/forums/viewtopic.php?t=27812

--- testcase.php
+++ testcase.php
@@ -2,8 +2,19 @@
 
 require_once __DIR__ . '/smarty-config.php';
 
+$smarty->assign("a", new class
+{
+  function b()
+  {
+    return ["c"];
+  }
+});
+
 $smarty->display(__FILE__);
 
 __HALT_COMPILER();
-?>PHP: {$smarty.const.PHP_VERSION}
+?>{$d=$a->b()}{$d[0]}
+{* Parsing error $a->b()[0] *}
+
+PHP: {$smarty.const.PHP_VERSION}
 Smarty: {$smarty.version}
