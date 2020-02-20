2331a04bfd10dd151b652b84ebb9e4bf8d27b5be
AnrDaemon <anrdaemon@yandex.ru>
2020-02-20 23:18:52 +0300

    Test case for smarty-php/smarty#457
    Ref: https://github.com/smarty-php/smarty/issues/457

--- testcase.php
+++ testcase.php
@@ -2,8 +2,28 @@
 
 require_once __DIR__ . '/smarty-config.php';
 
+class SvgImage {
+    public function render($params, $smarty) {
+        $params += [
+            'id' => null,
+            'class' => null,
+            'style' => null,
+            'fallback' => null,
+        ];
+        return "\$" . __CLASS__ . " = " . var_export($params, true);
+    }
+}
+
+$smarty->registerPlugin('function', 'svgImage', [new SvgImage('/img/cat/icon'), 'render'], true, [
+    'name', 'id', 'class', 'style', 'fallback'
+]);
+
 $smarty->display(__FILE__);
 
 __HALT_COMPILER();
-?>PHP: {$smarty.const.PHP_VERSION}
+?>{svgImage name="icon"}
+
+{svgImage name="icon" id="id" class="none" style="float: left;" fallback="text"}
+
+PHP: {$smarty.const.PHP_VERSION}
 Smarty: {$smarty.version}
