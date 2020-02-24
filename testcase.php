219c41113c08ec0604cf05ada44661867f468d1a
AnrDaemon <anrdaemon@yandex.ru>
2020-02-24 07:00:46 +0300

    Testcase for smarty-php/smarty#497

--- testcase.php
+++ testcase.php
@@ -2,8 +2,17 @@
 
 require_once __DIR__ . '/smarty-config.php';
 
-$smarty->display(__FILE__);
+$template = $smarty->createTemplate(__FILE__);
+$template->setCacheLifetime(1);
+
+if($template->isCached() ){
+	die('Template is cached');
+} else {
+	$template->display();
+}
 
 __HALT_COMPILER();
 ?>PHP: {$smarty.const.PHP_VERSION}
 Smarty: {$smarty.version}
+
+{$smarty.now}
